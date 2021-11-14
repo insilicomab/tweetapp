2b504432f388b7d7df448cefeb81d05b8647adb6
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-11-14 18:19:56 +0900

    モデル作成、管理サイトに登録

--- post/models.py
+++ post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'posts'
+
+
+class Likes(models.Model):
+    post = models.ForeignKey('post.Posts', on_delete=models.CASCADE)
+    user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'likes'
