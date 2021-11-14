83b1e98b55331ff357a2e6f342bc8368515854e9
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-11-14 13:10:55 +0900

    更新画面の画像フォームの編集

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Users
 from django.contrib.auth.password_validation import validate_password
+from django.forms.widgets import FileInput
 
 
 class RegistForm(forms.ModelForm):
