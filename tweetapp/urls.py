ba8e0eb4abd8f2e0a87b7356cfc60b2836af90cb
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-11-25 18:40:52 +0900

    urlの変更

--- tweetapp/urls.py
+++ tweetapp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('post/', include('post.urls')),
 ]
 
