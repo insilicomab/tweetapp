2fd7219b46457550d6790bef5b28e598e261c2ac
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-01-09 23:39:42 +0900

    テスト追加

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+
+# Topページ
+class TopPageRenderTest(TestCase):
+    def test_should_return_top_page_title(self):
+        response = self.client.get("/")
+        self.assertContains(response, 'つぶやきで、世界はつながる', status_code=200)
+    
+    def test_should_use_expected_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'accounts/top.html')
+
+
+# Aboutページ
+class AboutPageRenderTest(TestCase):
+    def test_should_return_about_page_title(self):
+        response = self.client.get("/about")
+        self.assertContains(response, 'TweetAppとは', status_code=200)
+    
+    def test_should_use_expected_template(self):
+        response = self.client.get("/about")
+        self.assertTemplateUsed(response, 'accounts/about.html')
--- post/models.py
+++ post/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Posts(models.Model):
-    content = models.CharField(max_length=140)
-    user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField('ツイート内容', max_length=140)
+    user = models.ForeignKey('accounts.Users', verbose_name='ユーザー',on_delete=models.CASCADE)
+    created_at = models.DateTimeField('投稿日', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日', auto_now=True)
 
     class Meta:
         db_table = 'posts'
