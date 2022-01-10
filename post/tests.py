4d4342f7642aacc1896968d210fb3941cbaddcae
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-01-10 19:15:02 +0900

    テスト

--- post/tests.py
+++ post/tests.py
@@ -7,3 +7,17 @@ from .models import Posts
 UserModel = get_user_model()
 
 
+# モデルのテスト
+class PostManagerTest(TestCase):
+    def test_public_post_manager(self):
+        user = UserModel.objects.create_user(
+            username = 'test',
+            email = 'test@email.com',
+            password = 'password'
+        )
+        Posts.objects.create(content='content1', user=user)
+        Posts.objects.create(content='content2', user=user)
+        posts = Posts.objects.all()
+        self.assertEqual(len(posts), 2)
+        self.assertEqual(posts[0].content, 'content1')
+        self.assertEqual(posts[0].user.username, 'test')
